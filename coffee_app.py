f38a9b3b877a2a90f8c233ec47725051ef5773e3
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-06-27 10:44:43 +0545

    Refactor CoffeeApp class and fix initialization

--- coffee_app.py
+++ coffee_app.py
@@ -1,5 +1,5 @@
-class CoffeeApp:
-    def _init_(self):
+class CoffeeApp: 
+    def __init__(self): 
         self.menu = {
             "Espresso": 2.50,
             "Latte": 3.50,
@@ -7,56 +7,7 @@ class CoffeeApp:
             "Americano": 3.00,
             "Mocha": 4.00
         }
-        self.order = []
-
-    def display_menu(self):
-        print("\n-------Coffee Menu-------")
-        i = 1
-        for item, price in self.menu.item():
-            print(f"{i}. {item} - ${price:.2f}")
-            i += 1
-
-    def add_to_order(self):
-        self.display_menu()
-
-        try:
-            choice = int(inout("Enter coffee number: "))
-
-            if choice >= 1 and choice <= len(self.menu):
-                item = list(self.menu.keys())[choice - 1]
-                self.order.append(item)
-                print(item, "added to your order.")
-            else:
-                print("Invalid Choice.")
-
-        except ValueError:
-            print("Please enter a number.")
-    
-    def view_order(self):
-        if len(self.order) == 0:
-            print("\nYour order is empty.")
-            return False
-
-        print("\n-------Your Oreder------")
-        total = 0
-
-        for items in self.order:
-            print(item, "-$", self.menu[item])
-            total += self.menu[item]
-
-        print("Total = $",total)
-        return True
-
-class CoffeeApp: # Creates a new class called coffee app.It will hold the coffee shop's menu and orders
-    def __init__(self): # This tries to define the initializer (the method that runs when we create a new CoffeeApp object).
-        self.menu = {
-            "Espresso": 2.50,
-            "Latte": 3.50,
-            "Cappuccino": 3.75,
-            "Americano": 3.00,
-            "Mocha": 4.00
-        }
-        self.order = [] # Creates an empty list called order where selected coffee names will be stored.
+        self.order = [] 
 
     def display_menu(self):
         print("\n-------Coffee Menu-------")
